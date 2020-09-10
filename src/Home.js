2848724671d4dd536c738f8dcf19a802a782f3a2
bhargavi <ssreddyg123>
2020-09-10 15:32:12 +0100

    checkoutpage and removefrombasket functionality

--- src/Home.js
+++ src/Home.js
@@ -15,13 +15,13 @@ function Home() {
           <Product
             title="Car Mount Phone Holder Desk Stand"
             price={59.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/718NVofDrCL._AC_SL1500_.jpg"
+            image="https://m.media-amazon.com/images/I/61mKxEow1oL._AC_UL320_.jpg"
             rating={5}
           />
           <Product
             title="Lamaze Peek-A-Boo Forest, Fun Interactive Baby Book with Inspiring Rhymes and Stories"
             price={10}
-            image="https://images-na.ssl-images-amazon.com/images/I/91xxqc9IwcL._SL1500_.jpg"
+            image="https://m.media-amazon.com/images/I/91xxqc9IwcL._AC_UY218_.jpg"
             rating={4}
           />
 
@@ -33,19 +33,19 @@ function Home() {
           <Product
             title="RUNMUS Gaming Headset Xbox One Headset"
             price={38.95}
-            image="https://images-na.ssl-images-amazon.com/images/I/61lnzTv2a0L._AC_SL1000_.jpg"
+            image="https://m.media-amazon.com/images/I/61lnzTv2a0L._AC_UY218_.jpg"
             rating={4}
           />
           <Product
             title="AmazonBasics Small Digital Alarm Clock with Nightlight and Battery Backup, LED Display"
             price={9.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/61j17FjPhtL._AC_SX425_.jpg"
+            image="https://m.media-amazon.com/images/I/61inPwwE6sL._AC_SX180_SY120_QL70_.jpg"
             rating={5}
           />
           <Product
             title="Play-Doh Modeling Compound 10 Pack Case of Colors"
             price={7.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/81xV%2BD1OkGL._AC_SX569_.jpg"
+            image="https://m.media-amazon.com/images/I/81xV+D1OkGL._AC_UL320_.jpg"
             rating={3}
           />
           {/* Product */}
@@ -57,7 +57,7 @@ function Home() {
           <Product
             title="SAMSUNG FLAT SMART TV"
             price={497.99}
-            image="https://images-na.ssl-images-amazon.com/images/I/51NKhnjhpGL._AC_SL1024_.jpg"
+            image="https://m.media-amazon.com/images/I/51m-OJ7TQIL._AC_UY218_.jpg"
             rating={4}
           />
           {/* Product */}
